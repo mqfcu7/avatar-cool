c03d3dc1405ff254751b96e846de52db47ef5fbf
mqfcu7 <mqfcu7@gmail.com>
2018-08-03 09:55:09 +0800

    add floating button

--- app/src/main/java/com/mqfcu7/jiangmeilan/avatar/BuildAvatarActivity.java
+++ app/src/main/java/com/mqfcu7/jiangmeilan/avatar/BuildAvatarActivity.java
@@ -1,12 +1,19 @@
 package com.mqfcu7.jiangmeilan.avatar;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
+import com.mqfcu7.jiangmeilan.avatar.databinding.ActivityBuildAvatarBinding;
+
 public class BuildAvatarActivity extends AppCompatActivity {
+
+    private ActivityBuildAvatarBinding mBinding;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_build_avatar);
     }
 }
